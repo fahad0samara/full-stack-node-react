6e00b50b586e15d739bb0658279fb02733709f82
fahad <90055525+fahad0samara@users.noreply.github.com>
2022-12-10 14:21:11 +0200

    update the user

--- client/deashbrd/src/Patient/Dashbord.tsx
+++ client/deashbrd/src/Patient/Dashbord.tsx
@@ -10,18 +10,49 @@ import {
 import axios from "axios";
 import {useLogIN} from "../../ContextLog";
 import Table from "./Table";
+import {BiTime} from "react-icons/bi";
+import {FcCalendar} from "react-icons/fc";
+
 const Dashboard = (props: any) => {
-  const { log, setLog, Profile, setProfile, loading, setLoading,
-    dark, setdark
-  } = useLogIN();
+  const {log, setLog, Profile, setProfile, loading, setLoading, dark, setdark} =
+    useLogIN();
   const navigate = useNavigate();
+  const [data, setData] = useState<any>([]);
 
-  console.log(
-    "🚀 ~ file: Dashbord.tsx ~ line 10 ~ Dashboard ~ Profile",
-    Profile.name
-  );
+  // show message if morning
+  const [message, setMessage] = useState<string>("");
+  const [time, setTime] = useState<string>("");
+  const [date, setDate] = useState<string>("");
+  const [day, setDay] = useState<string>("");
+  const [month, setMonth] = useState<string>("");
+  const [year, setYear] = useState<string>("");
+  const [hour, setHour] = useState<string>("");
+  const [minute, setMinute] = useState<string>("");
 
-
+  useEffect(() => {
+    const date = new Date();
+    const day = date.getDay();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const time = `${hour}:${minute}`;
+    const date1 = `${day}/${month}/${year}`;
+    setTime(time);
+    setDate(date1);
+    setDay(day.toString());
+    setMonth(month.toString());
+    setYear(year.toString());
+    setHour(hour.toString());
+    setMinute(minute.toString());
+    if (hour >= 0 && hour < 12) {
+      setMessage("Good Morning");
+    } else if (hour >= 12 && hour < 17) {
+      setMessage("Good Afternoon");
+    } else if (hour >= 17 && hour < 24) {
+      setMessage("Good Evening");
+    }
+  }, []);
 
   const convertDate = (date: any) => {
     const newDate = new Date(date);
@@ -32,7 +63,7 @@ const Dashboard = (props: any) => {
   };
 
   return (
-    <div className="ml-16">
+    <div className="xl:ml-16 ml-12">
       <div
         style={{
           backgroundColor: dark ? "#000" : "white",
@@ -40,184 +71,259 @@ const Dashboard = (props: any) => {
         }}
         className=""
       >
-        <div className="  ">
+        {
+          // name and profile
+        }
+        <div className=" grid sm:grid-cols-2 xl:mx-12 grid-cols-1  ">
+          <div className="flex flex-col justify-center p-4 mt-8 ">
+            <h1 className="xl:text-3xl md:text-2xl text-xl font-bold text-cyan-300  ">
+              <span
+                style={{
+                  backgroundColor: dark ? "#000" : "white",
+                  color: dark ? "white" : "black",
+                }}
+                className="xl:text-3xl md:text-2xl text-xl font-bold mx-2"
+              >
+                {message}
+              </span>
+
+              {Profile ? Profile.name.firstName : ""}
+            </h1>
+            <h4 className="text-sm font-bold mt-1 ml-3 text-zinc-400  ">
+              You'll be feeling healthy and strong again soon!
+            </h4>
+          </div>
+
+          <div className="flex items-center mt-8 xl:ml-36 md:ml-28 mx-16   drop-shadow-lg   ">
+            <div className="flex flex-col items-center mx-3">
+              <span className="text-xl mx-2 shadow-cyan-300 shadow-xl ">
+                <FcCalendar />
+              </span>
+              <span className="text-xl  font-semibold">{date}</span>
+            </div>
+            <div className="flex flex-col items-center mx-4">
+              <span className="text-xl mx-2 shadow-cyan-300 shadow-xl ">
+                <BiTime />
+              </span>
+              <span className="text-xl font-semibold">{time}</span>
+            </div>
+          </div>
+        </div>
+
+        <div
+          style={{
+            backgroundColor: dark ? "#000" : "white",
+            color: dark ? "white" : "black",
+          }}
+          className="grid xl:grid-cols-3 md:grid-cols-2 mt-10 grid-cols-1 gap-2  md:mx-10  "
+        >
+          {
+            //   Patient Details
+          }
           <div
             style={{
               backgroundColor: dark ? "#000" : "white",
               color: dark ? "white" : "black",
+              boxShadow: dark
+                ? "0px 0px 10px 0px rgb(103 232 249)"
+                : "0px 0px 10px 0px #ccc",
             }}
-            className="flex flex-wrap"
-            
+            className="p-5 rounded-2xl ml-7 w-72 md:ml-4 my-2 sm:my-0   md:w-80 "
           >
-            <div className="relative flex flex-col min-w-0 mb-6 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border">
-              <div className="flex-auto p-4">
-                <div className="flex flex-wrap -mx-3">
-                  <div className="flex-none w-2/3 max-w-full px-3">
-                    <div>
-                      <p className="mb-0 font-sans font-semibold leading-normal text-sm">
-                        Today's Money
-                      </p>
-                      <h5 className="mb-0 font-bold">
-                        $53,000
-                        <span className="leading-normal text-sm font-weight-bolder text-lime-500">
-                          +55%
-                        </span>
-                      </h5>
-                    </div>
-                  </div>
-                  <div className="w-4/12 max-w-full px-3 ml-auto text-right flex-0">
-                    <div className="inline-block w-12 h-12 text-center rounded-lg bg-gradient-to-tl from-purple-700 to-pink-500 shadow-soft-2xl">
-                      <i
-                        className="ni ni-money-coins text-lg relative top-3.5 text-white"
-                        aria-hidden="true"
-                      ></i>
-                    </div>
-                  </div>
-                </div>
+            <div className="flex flex-col justify-start">
+              <h1 className="text-xl font-bold text-cyan-300  ">
+                Patient Details
+              </h1>
+              <div className="flex flex-row justify-start mt-4 ">
+                <h1 className="text-md font-bold text-zinc-400 italic mx-1  ">
+                  Name:
+                </h1>
+                <h1
+                  className="
+                   font-extrabold  italic  ml-1
+                 "
+                >
+                  {Profile ? Profile.name.firstName : ""}
+                </h1>
               </div>
+              <div className="flex flex-row justify-start mx-1  ">
+                <h1 className="text-md font-bold text-zinc-400 italic   ">
+                  middleName :
+                </h1>
+                <h1
+                  className="
+                   font-extrabold  italic  ml-1
+                 "
+                >
+                  {Profile ? Profile.name.middleName : ""}
+                </h1>
+              </div>
+              <div className="flex flex-row justify-start mx-1 ">
+                <h1 className="text-md font-bold text-zinc-400 italic   ">
+                  lastName :
+                </h1>
+                <h1
+                  className="
+                   font-extrabold  italic  ml-1
+                 "
+                >
+                  {Profile ? Profile.name.LastName : ""}
+                </h1>
+              </div>
+              <div className="flex flex-row justify-start  ">
+                <h1 className="text-md font-bold text-zinc-400 italic mx-1  ">
+                  Date of Birth :
+                </h1>
+                <h1
+                  className="
+                   font-extrabold  italic  ml-1
+                 "
+                >
+                  {Profile ? convertDate(Profile.date) : ""}
+                </h1>
+              </div>
+              <div className="flex flex-col justify-start mt-4"></div>
             </div>
-            <div className="relative flex flex-col min-w-0 mb-6 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border">
-              <div className="flex-auto p-4">
-                <div className="flex flex-wrap -mx-3">
-                  <div className="flex-none w-2/3 max-w-full px-3">
-                    <div>
-                      <p className="mb-0 font-sans font-semibold leading-normal text-sm">
-                        Today's Money
-                      </p>
-                      <h5 className="mb-0 font-bold">
-                        $53,000
-                        <span className="leading-normal text-sm font-weight-bolder text-lime-500">
-                          +55%
-                        </span>
-                      </h5>
-                    </div>
-                  </div>
-                  <div className="w-4/12 max-w-full px-3 ml-auto text-right flex-0">
-                    <div className="inline-block w-12 h-12 text-center rounded-lg bg-gradient-to-tl from-purple-700 to-pink-500 shadow-soft-2xl">
-                      <i
-                        className="ni ni-money-coins text-lg relative top-3.5 text-white"
-                        aria-hidden="true"
-                      ></i>
-                    </div>
-                  </div>
-                </div>
+          </div>
+          <div
+            style={{
+              backgroundColor: dark ? "#000" : "white",
+              color: dark ? "white" : "black",
+              boxShadow: dark
+                ? "0px 0px 10px 0px rgb(103 232 249)"
+                : "0px 0px 10px 0px #ccc",
+            }}
+            className="p-5 rounded-2xl ml-7 w-72 md:ml-4 my-4 sm:my-0   md:w-80 "
+          >
+            <div className="flex flex-col justify-start">
+              <div className="flex flex-row justify-start mt-4 ">
+                <h1 className="text-md font-bold text-zinc-400 italic mx-1  ">
+                  healthIDNumber
+                </h1>
+                <h1
+                  className="
+                   font-extrabold text-cyan-400  italic  ml-1
+                 "
+                >
+                  {Profile ? Profile.healthIDNumber : ""}
+                </h1>
+              </div>
+              <div className="flex flex-row justify-start mx-1 my-1  ">
+                <h1 className="text-md font-bold text-zinc-400 italic   ">
+                  mobile :
+                </h1>
+                <h1
+                  className="
+                   font-extrabold  italic  ml-1
+                 "
+                >
+                  {Profile ? Profile.mobile : ""}
+                </h1>
+              </div>
+              <div className="flex flex-row justify-start mx-1 ">
+                <h1 className="text-md font-bold text-zinc-400 italic   ">
+                  email :
+                </h1>
+                <h1
+                  className="
+                   font-extrabold  italic  ml-1
+                 "
+                >
+                  {Profile ? Profile.email : ""}
+                </h1>
+              </div>
+
+              <div className="flex flex-row justify-start my-1  ">
+                <h1 className="text-md font-bold text-zinc-400 italic mx-1  ">
+                  bloodGroup
+                </h1>
+                <h1
+                  className="
+                   font-extrabold  italic  ml-1 
+                 "
+                >
+                  {Profile ? Profile.bloodGroup : ""}
+                </h1>
+              </div>
+              <div className="flex flex-col justify-start mt-4"></div>
+            </div>
+          </div>
+          <div
+            style={{
+              backgroundColor: dark ? "#000" : "white",
+              color: dark ? "white" : "black",
+              boxShadow: dark
+                ? "0px 0px 10px 0px rgb(103 232 249)"
+                : "0px 0px 10px 0px #ccc",
+            }}
+            className="p-5 rounded-2xl ml-7 w-72 md:ml-4 my-2 sm:my-0   md:w-80 "
+          >
+            <h1 className="text-xl font-bold text-cyan-300  ">
+              Emergency contact person
+            </h1>
+            <div className="flex flex-col justify-start mt-4 ">
+              {
+                //firstName and LastName
+              }
+              <div className="flex flex-row justify-start ">
+                <h1 className="text-md font-bold text-zinc-400 italic mx-1  ">
+                  Name:
+                </h1>
+                <h1
+                  className="
+                    font-extrabold italic ml-1"
+                >
+                  {Profile ? Profile.contactPerson.name.firstName : ""}
+                </h1>
+                <h1
+                  className="
+                    font-extrabold italic ml-1"
+                >
+                  {Profile ? Profile.contactPerson.name.LastName : ""}
+                </h1>
+              </div>
+
+              <div className="flex flex-row justify-start mx-1 my-1  ">
+                <h1 className="text-md font-bold text-zinc-400 italic   ">
+                  mobile :
+                </h1>
+                <h1
+                  className="
+                    font-extrabold  italic  ml-1
+                  "
+                >
+                  {Profile ? Profile.contactPerson.mobile : ""}
+                </h1>
+              </div>
+              <div className="flex flex-row justify-start mx-1 ">
+                <h1 className="text-md font-bold text-zinc-400 italic   ">
+                  email :
+                </h1>
+                <h1
+                  className="
+                    font-extrabold  italic  ml-1
+                  "
+                >
+                  {Profile ? Profile.contactPerson.email : ""}
+                </h1>
+              </div>
+              <div className="flex flex-row justify-start my-1  ">
+                <h1 className="text-md font-bold text-zinc-400 italic mx-1  ">
+                  relationship
+                </h1>
+                <h1
+                  className="
+                    font-extrabold  italic  ml-1
+                  "
+                >
+                  {Profile ? Profile.contactPerson.relation : ""}
+                </h1>
               </div>
             </div>
           </div>
-
-          <div className="grid">
-            <div className="m-4 p-4">
-              <div>
-                <h1 className="font-bold font-poppins text-xl ">
-                  Patient Details
-                </h1>
-              </div>
-              <div className="bg-white font-poppins p-4 mt-4 px-8 rounded-xl shadow">
-                <div className="flex">
-                  <div>
-                    <h1>Name : </h1>
-                  </div>
-
-                  <div className="flex ml-2   ">
-                    <h1 className="pl-1">
-                      {Profile ? Profile.name.firstName : ""}
-                    </h1>
-
-                    <h1 className="pl-1">
-                      {Profile ? Profile.name.middleName : ""}
-                    </h1>
-                    <h1 className="pl-1">
-                      {Profile ? Profile.name.LastName : ""}
-                    </h1>
-                  </div>
-                </div>
-                <div className="flex">
-                  <div>
-                    <h1>Date : </h1>
-                  </div>
-                  <div className="ml-2">
-                    <h1>{Profile ? convertDate(Profile.date) : ""}</h1>
-                  </div>
-                </div>
-                <div className="flex">
-                  <div>
-                    <h1>Blood group : </h1>
-                  </div>
-                  <div className="ml-2">
-                    <h1>{Profile ? Profile.bloodGroup : ""}</h1>
-                  </div>
-                </div>
-                <div>
-                  <h1 className="font-bold mt-4">Past Health History</h1>
-                  <div>{`${
-                    //diseaseList
-                    Profile ? Profile.diseaseList[0].disease : ""
-                  } (${Profile ? Profile.diseaseList[0].YearRound : ""})`}</div>
-                  <div>{`${Profile ? Profile.allergyList[0].allergy : ""} (${
-                    Profile ? Profile.allergyList[0].YearRound : ""
-                  })`}</div>
-
-                  <div>{`${
-                    Profile ? Profile.medicationList[0].medication : ""
-                  } (${
-                    Profile ? Profile.medicationList[0].YearRound : ""
-                  })`}</div>
-                </div>
-              </div>
-            </div>
-            {/* recent health check up start */}
-            <div className="m-4 p-4 ">
-              <div>
-                <h1 className="font-bold font-poppins text-xl ">
-                  Recent Health Checkup
-                </h1>
-              </div>
-              {/* {Profile
-                ? Profile.checkupList.map((mappedData: any) => {
-                    return (
-                      <div className="bg-white mt-4 font-poppins p-4 rounded-xl shadow px-8">
-                        <div className="flex ">
-                          <div>
-                            <h1>Consultant Doctor :</h1>
-                          </div>
-                          <div className="ml-2">
-                            <h1>{`Dr. ${mappedData.doctor}`}</h1>
-                          </div>
-                        </div>
-                        <div className="flex">
-                          <div>
-                            <h1>Date :</h1>
-                          </div>
-                          <div className="ml-2">
-                            <h1>{convertDate(mappedData.createdAt)}</h1>
-                          </div>
-                        </div>
-                        <div className="flex">
-                          <div>
-                            <h1>Diagnosis :</h1>
-                          </div>
-                          <div className="ml-2">
-                            <h1>{mappedData.diagnosis}</h1>
-                          </div>
-                        </div>
-                        <div className="flex">
-                          <div>
-                            <h1>Prescription :</h1>
-                          </div>
-                          <div className="ml-2">
-                            <h1>{mappedData.prescription}</h1>
-                          </div>
-                        </div>
-                      </div>
-                    );
-                  })
-                : ""}
-            </div> */}
-              {/* recent health check up end */}
-            </div>
-          </div>
-          <Table />
         </div>
+
+        <Table />
       </div>
     </div>
   );
